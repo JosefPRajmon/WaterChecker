9f57fed356144848949306fb4d03ac02ca158516
Josef Rajmon <rajmon.josef1999@seznam.cz>
2024-12-30 08:52:37 +0100

    create class controler for health connect

---
+++ ClassFunction/HealthConnectControler.cs
@@ -0,0 +1,52 @@
+﻿#if ANDROID
+using Android.Content;
+using Android.Content.PM;
+using Android.Health.Connect;
+using Android.Health.Connect.DataTypes;
+using Android.Health.Connect.DataTypes.Units;
+using AndroidX.Health.Connect.Client;
+using AndroidX.Health.Connect.Client.Records;
+using Microsoft.VisualBasic;
+#endif
+
+namespace WaterChecker.ClassFunction
+{
+#if ANDROID
+    public class HealthConnectControler
+    {
+        public async void GetPermision()
+        {
+            if (OperatingSystem.IsAndroidVersionAtLeast(28))
+            {
+
+
+                try
+                {
+                    var activity = Platform.CurrentActivity;
+
+                    // Správný intent pro Health Connect permissions
+
+                    try
+                    {
+                        var intent = new Intent("androidx.health.ACTION_HEALTH_CONNECT_SETTINGS");
+                        // Spuštění aktivity pro získání permissions
+                        activity.StartActivity(intent);
+                    }
+                    catch (ActivityNotFoundException ex)
+                    {
+                        // Health Connect není nainstalován
+                        // Můžeme uživatele přesměrovat do Google Play Store
+                        var playStoreIntent = new Intent(Intent.ActionView);
+                        playStoreIntent.SetData(Android.Net.Uri.Parse("market://details?id=com.google.android.apps.healthdata"));
+                        activity.StartActivity(playStoreIntent);
+                    }
+                }
+                catch (Exception)
+                {
+                }
+
+            }
+        }
+    }
+#endif
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,17 +1,13 @@
 ﻿#if ANDROID
-using Android.Content;
-using Android.Content.PM;
-using Android.Health.Connect;
-using Android.Health.Connect.DataTypes;
-using Android.Health.Connect.DataTypes.Units;
-using AndroidX.Health.Connect.Client;
-using AndroidX.Health.Connect.Client.Records;
-using Microsoft.VisualBasic;
+using WaterChecker.ClassFunction;
 #endif
 namespace WaterChecker
 {
     public partial class MainPage : ContentPage
     {
+#if ANDROID
+        HealthConnectControler HealthConnectControler = new HealthConnectControler();
+#endif
         public MainPage()
         {
             InitializeComponent();
@@ -27,36 +23,7 @@ namespace WaterChecker
         private async void HealthConnectPermision_Button(object sender, EventArgs e)
         {
 #if ANDROID
-            if (OperatingSystem.IsAndroidVersionAtLeast(28))
-            {
-
-
-                try
-                {
-                    var activity = Platform.CurrentActivity;
-
-                    // Správný intent pro Health Connect permissions
-
-                    try
-                    {
-                        var intent = new Intent("androidx.health.ACTION_HEALTH_CONNECT_SETTINGS");
-                        // Spuštění aktivity pro získání permissions
-                        activity.StartActivity(intent);
-                    }
-                    catch (ActivityNotFoundException ex)
-                    {
-                        // Health Connect není nainstalován
-                        // Můžeme uživatele přesměrovat do Google Play Store
-                        var playStoreIntent = new Intent(Intent.ActionView);
-                        playStoreIntent.SetData(Android.Net.Uri.Parse("market://details?id=com.google.android.apps.healthdata"));
-                        activity.StartActivity(playStoreIntent);
-                    }
-                }
-                catch (Exception)
-                {
-                }
-
-            }
+            HealthConnectControler.GetPermision();
 #endif
         }
     }
