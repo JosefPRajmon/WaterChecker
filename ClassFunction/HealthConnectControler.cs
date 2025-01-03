6a1c383b9a462c61cb15bbd41f369498b5f193cf
Josef Rajmon <rajmon.josef1999@seznam.cz>
2025-01-03 19:54:46 +0100

    trying fix problems

--- ClassFunction/HealthConnectControler.cs
+++ ClassFunction/HealthConnectControler.cs
@@ -5,6 +5,7 @@ using Android.Health.Connect;
 using Android.Health.Connect.DataTypes;
 using Android.Health.Connect.DataTypes.Units;
 using Android.OS;
+using AndroidX.Core.OS;
 using AndroidX.Health.Connect.Client;
 using AndroidX.Health.Connect.Client.Records;
 using Java.Time;
@@ -60,18 +61,15 @@ namespace WaterChecker.ClassFunction
         {
             // creating record
             IList<Record> records = new List<HydrationRecord>
-            {//new HydrationRecord.Builder( new Metadata.Builder().Build(),Instant.Now(),Instant.Now(),Volume.FromLiters(0.5)).Build()
+            {new HydrationRecord.Builder( new Metadata.Builder().Build(),Instant.Now(),Instant.Now(),Volume.FromLiters(0.5)).Build()
 
             };
 
-            // Executor for control operacion
-            IExecutor executor = new ExecutionContext(); // Nahraďte konkrétní implementací
-
-            // Callback for result
-            IOutcomeReceiver callback = new MyOutcomeReceiver(); // Nahraďte konkrétní implementací
+            var executor = Executors.NewSingleThreadExecutor();
+            var outcomeReceiver = new OutcomeReceiverKt();
 
             // call method
-            HealthConnectManager.InsertRecords(records, executor, callback);
+            HealthConnectManager.InsertRecords(records, executor, outcomeReceiver);
 
         }
         public void GetHydratacion()
